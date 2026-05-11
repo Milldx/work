0796d32d22014b543a1c23b0971c6ff2944f7285
Milldx <maxim.moi@icloud.com>
2026-05-11 23:49:26 +0300

    fix: router hashHistory, add exercise route, fix title

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import SurveyView from '../views/SurveyView.vue'
@@ -10,15 +10,17 @@ import ProfileView from '../views/ProfileView.vue'
 import NotFoundView from '../views/NotFoundView.vue'
 import PayView from '../views/PayView.vue'
 import SupplementsView from '../views/SupplementsView.vue'
+import ExerciseView from '../views/ExerciseView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     { path: '/',          component: HomeView },
     { path: '/register',  component: RegisterView },
     { path: '/survey',    component: SurveyView },
     { path: '/login',     component: LoginView },
     { path: '/workout',   component: WorkoutView },
+    { path: '/exercise/:id', component: ExerciseView },
     { path: '/nutrition', component: NutritionView },
     { path: '/supplements', component: SupplementsView },
     { path: '/pay',       component: PayView },
